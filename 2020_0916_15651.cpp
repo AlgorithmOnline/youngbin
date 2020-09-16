a5f4ca57a1c5a8299d31718c8da7d415e6d674f7
Young Bin Go <skekq123@naver.com>
2020-09-16 22:04:55 +0900

    BOJ_N과 M (3)

---
+++ 2020_0916_15651.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int N, M;
+void dfs(int depth);
+vector<int> temp;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> N >> M;
+	dfs(0);
+
+	return 0;
+}
+void dfs(int depth)
+{
+	if (depth == M)
+	{
+		for (int i = 0; i < M; ++i)
+		{
+			cout << temp[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = 1; i <= N; ++i)
+	{
+		temp.push_back(i);
+		dfs(depth + 1);
+		temp.pop_back();
+	}
+}
